8d867fbdb3b3e5732d7db77c71838fba107d2d9a
priyankshukla <priyank.shukla@eko.co.in>
2019-04-30 23:42:23 +0530

    first commit

---
+++ src/main/java/com/eko/maven/MavenProj/App.java
@@ -0,0 +1,13 @@
+package com.eko.maven.MavenProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
